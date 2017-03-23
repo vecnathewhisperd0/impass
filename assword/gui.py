2e7d7a1560b0a53761134ca8023a405d5b569624
Daniel Kahn Gillmor <dkg@fifthhorseman.net>
2017-03-23 10:21:50 -0400

    Conversion to GtkBuilder
    This changes how we're doing the layout, should make it simpler and
    clearer to add new gui features in the future.
    
    We also take this opportunity to make the button just show what's
    going to happen when the user hits enter, rather than being disabled
    whenever it's not "Create"

--- assword/gui.py
+++ assword/gui.py
@@ -14,11 +14,125 @@ def _match_func(completion, key, iter, column):
     if text.lower().find(key.lower()) > -1:
         return True
     return False
+_gui_layout = '''<?xml version="1.0" encoding="UTF-8"?>
+<!-- Generated with glade 3.20.0 -->
+<interface>
+  <requires lib="gtk+" version="3.20"/>
+  <object class="GtkWindow" id="assword-gui">
+    <property name="can_focus">False</property>
+    <property name="border_width">4</property>
+    <property name="title" translatable="yes">assword</property>
+    <property name="icon_name">dialog-password</property>
+    <child>
+      <object class="GtkBox">
+        <property name="visible">True</property>
+        <property name="can_focus">False</property>
+        <property name="orientation">vertical</property>
+        <child>
+          <object class="GtkBox" id="warning">
+            <property name="visible">False</property>
+            <property name="can_focus">False</property>
+            <property name="orientation">vertical</property>
+            <child>
+              <object class="GtkLabel" id="notification">
+                <property name="visible">True</property>
+                <property name="can_focus">False</property>
+                <property name="label" translatable="yes">WARNING: could not validate signature on db file!</property>
+                <attributes>
+                  <attribute name="foreground" value="#cccc00000000"/>
+                </attributes>
+              </object>
+              <packing>
+                <property name="expand">False</property>
+                <property name="fill">True</property>
+                <property name="position">0</property>
+              </packing>
+            </child>
+            <child>
+              <object class="GtkSeparator">
+                <property name="visible">True</property>
+                <property name="can_focus">False</property>
+              </object>
+              <packing>
+                <property name="expand">False</property>
+                <property name="fill">True</property>
+                <property name="position">1</property>
+              </packing>
+            </child>
+          </object>
+          <packing>
+            <property name="expand">False</property>
+            <property name="fill">True</property>
+            <property name="position">0</property>
+          </packing>
+        </child>
+        <child>
+          <object class="GtkLabel" id="description">
+            <property name="visible">True</property>
+            <property name="can_focus">False</property>
+            <property name="label">Global state of assword gui</property>
+          </object>
+          <packing>
+            <property name="expand">True</property>
+            <property name="fill">True</property>
+            <property name="position">1</property>
+          </packing>
+        </child>
+        <child>
+          <object class="GtkBox" id="simplebox">
+            <property name="visible">True</property>
+            <property name="can_focus">False</property>
+            <child>
+              <object class="GtkEntry" id="simplectxentry">
+                <property name="visible">True</property>
+                <property name="can_focus">True</property>
+                <property name="width_chars">50</property>
+                <property name="placeholder_text" translatable="yes">Enter context…</property>
+              </object>
+              <packing>
+                <property name="expand">True</property>
+                <property name="fill">True</property>
+                <property name="position">0</property>
+              </packing>
+            </child>
+            <child>
+              <object class="GtkButton" id="simplebtn">
+                <property name="label" translatable="yes">Emit</property>
+                <property name="visible">True</property>
+                <property name="can_focus">True</property>
+                <property name="receives_default">True</property>
+              </object>
+              <packing>
+                <property name="expand">False</property>
+                <property name="fill">True</property>
+                <property name="position">1</property>
+              </packing>
+            </child>
+          </object>
+          <packing>
+            <property name="expand">False</property>
+            <property name="fill">True</property>
+            <property name="position">2</property>
+          </packing>
+        </child>
+      </object>
+    </child>
+  </object>
+</interface>
+'''
 
 class Gui:
     """Assword X-based query UI."""
     def __init__(self, db, query=None):
-
+        '''
++--------------------- warning --------------------+
+|                    notification                  |
++--------------------------------------------------+
+|                    description                   |
++----------------- simplebox ----------------------+
+| [_simplectxentry___________________] <simplebtn> |
++--------------------------------------------------+
+'''
         self.db = db
         self.query = None
         self.results = None
@@ -41,14 +155,15 @@ class Gui:
                 self.selected = r[list(r.keys())[0]]
                 return
 
-        self.window = Gtk.Window(Gtk.WindowType.TOPLEVEL)
-        self.window.set_border_width(4)
-        windowicon = self.window.render_icon(Gtk.STOCK_DIALOG_AUTHENTICATION, Gtk.IconSize.DIALOG)
-        self.window.set_icon(windowicon)
+        self.builder = Gtk.Builder.new_from_string(_gui_layout, len(_gui_layout))
+        self.window = self.builder.get_object('assword-gui')
+        self.entry = self.builder.get_object('simplectxentry')
+        self.simplebtn = self.builder.get_object('simplebtn')
+        self.warning = self.builder.get_object('warning')
 
-        self.entry = Gtk.Entry()
-        if query:
-            self.entry.set_text(query)
+        if self.db.sigvalid is False:
+            self.warning.show()
+        
         completion = Gtk.EntryCompletion()
         self.entry.set_completion(completion)
         liststore = Gtk.ListStore(GObject.TYPE_STRING)
@@ -60,55 +175,51 @@ class Gui:
             if len(context) > context_len:
                 context_len = len(context)
             liststore.append([context])
-        hbox = Gtk.HBox()
-        vbox = Gtk.VBox()
-        self.button = Gtk.Button("Create")
-        self.label = Gtk.Label(label="enter context for desired password:")
-        self.window.add(vbox)
-
-        if self.db.sigvalid is False:
-            notification = Gtk.Label()
-            msg = "WARNING: could not validate signature on db file"
-            notification.set_markup('<span foreground="red">%s</span>' % msg)
-            if len(msg) > context_len:
-                context_len = len(msg)
-            hsep = Gtk.HSeparator()
-            vbox.add(notification)
-            vbox.add(hsep)
-            notification.show()
-            hsep.show()
-
-        vbox.add(self.label)
-        vbox.pack_end(hbox, False, False, 0)
-        hbox.add(self.entry)
-        hbox.pack_end(self.button, False, False, 0)
-        self.entry.set_width_chars(context_len)
-        self.entry.connect("activate", self.retrieve)
-        self.entry.connect("changed", self.update_button)
-        self.button.connect("clicked", self.create)
         self.window.connect("destroy", self.destroy)
         self.window.connect("key-press-event", self.keypress)
-    
-        self.entry.show()
-        self.label.show()
-        vbox.show()
-        hbox.show()
-        self.button.show()
-        self.update_button(self.entry)
+        self.entry.connect("activate", self.simpleclicked)
+        self.entry.connect("changed", self.update_simple_context_entry)
+        self.simplebtn.connect("clicked", self.simpleclicked)
+
+        if query:
+            self.entry.set_text(query)
+        self.set_state('Enter context for desired password:')
+        self.update_simple_context_entry(None)
         self.window.show()
 
+    def set_state(self, state):
+        self.builder.get_object('description').set_label(state)
+        
+    def update_simple_context_entry(self, widget):
+        sctx = self.entry.get_text()
+
+        if sctx in self.db:
+            self.simplebtn.set_label("Emit")
+            self.simplebtn.set_sensitive(True)
+        elif sctx is None or sctx == '':
+            self.simplebtn.set_label("Emit")
+            self.simplebtn.set_sensitive(False)
+        else:
+            self.simplebtn.set_label("Create")
+            self.simplebtn.set_sensitive(True)
+
+    def simpleclicked(self, widget):
+        sctx = self.entry.get_text()
+        if sctx in self.db:
+            self.retrieve(None)
+        elif sctx is None or sctx == '':
+            pass
+        else:
+            self.create(None)
+        
     def keypress(self, widget, event):
         if event.keyval == Gdk.KEY_Escape:
             Gtk.main_quit()
 
-    def update_button(self, widget, data=None):
-        e = self.entry.get_text()
-        self.button.set_sensitive(e != '' and e not in self.db)
-
     def retrieve(self, widget, data=None):
-        e = self.entry.get_text()
-        if e in self.db:
-            self.selected = self.db[e]
+        sctx = self.entry.get_text()
+        if sctx in self.db:
+            self.selected = self.db[sctx]
             if self.selected is None:
                 self.label.set_text("weird -- no context found even though we thought there should be one")
             else:
@@ -117,8 +228,8 @@ class Gui:
             self.label.set_text("no match")
 
     def create(self, widget, data=None):
-        e = self.entry.get_text()
-        self.selected = self.db.add(e)
+        sctx = self.entry.get_text()
+        self.selected = self.db.add(sctx)
         self.db.save()
         Gtk.main_quit()
 
