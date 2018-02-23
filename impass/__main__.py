e525df345f78af4b6f74f36241e64d3470cd6950
Jameson Graef Rollins <jrollins@finestructure.net>
2018-02-23 03:36:40 -0800

    transition: create symlink to satisfy dkg

--- impass/__main__.py
+++ impass/__main__.py
@@ -569,9 +569,10 @@ def main():
     if os.path.exists(assword_dir) and os.path.isdir(assword_dir) and not os.getenv('IMPASS_DB'):
         try:
             os.rename(assword_dir, IMPASS_DIR)
-            print("renamed ~/.assword -> ~/.impass", file=sys.stderr)
         except:
             sys.exit("Could not rename old assword directory ~/.assword -> ~/.impass.\nPlease check ~/.impass path.")
+        os.symlink(IMPASS_DIR, assword_dir)
+        print("renamed ~/.assword -> ~/.impass", file=sys.stderr)
     ### DEPRECATE
 
     cmd = sys.argv[1]
